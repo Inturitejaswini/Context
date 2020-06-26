49b59aec37c91647767e439a0c954abcd5ce1b99
Inturitejswini <tejaswinichowdary583@gmail.com>
2020-06-26 08:05:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/nav';
+import ProductList from './components/productList';
+import {Switch,Route} from 'react-router-dom'
 class App extends Component{
   render() {
     return (
       <div className="App">
-        <NavBar/>
+        <Route patch='/' component={NavBar}/>
+        <Switch>
+          <Route exact path='/' component={ProductList}/>
+        </Switch>
        <h1>hello world</h1>
       </div>
     )
---
+++ src/components/productList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { ProductConsumer } from '../contextAPI'
+import Product from '../components/product'
+class ProductList extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div>
+                    <h1>Find Your Cras</h1>
+                </div>
+                <div className="row">
+                    <ProductConsumer>
+                        {(value) => {
+                          return value.product.map(product =>{
+                              return <Product key={product.id} product={product}/>
+                          })
+                        }}
+                    </ProductConsumer>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ProductList;
